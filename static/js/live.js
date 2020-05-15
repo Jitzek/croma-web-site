ea892c45da5bbd90f0b0d65af58f97a6c005b4e0
Jitzek <Jitzek@users.noreply.github.com>
2020-05-15 19:29:06 +0200

    Update live.js

--- static/js/live.js
+++ static/js/live.js
@@ -1,7 +1,6 @@
 var player = document.getElementById('playerDiv');
 
 function displayMSG( msg, color, code=null) {
-    // Susceptible to XSS, do not give variable access to users
     player.innerHTML = '';
     if (code) {player.innerHTML =+ `<h2>${code ? code : ''}</h2><br>`; }
     player.innerHTML +=
